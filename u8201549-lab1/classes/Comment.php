7a59e096848ae625a797cf6a8591bc62d38df05a
Юрий <89079756+YuriyLav@users.noreply.github.com>
2023-03-13 22:07:10 +0300

    Update Comment.php

--- u8201549-lab1/classes/Comment.php
+++ u8201549-lab1/classes/Comment.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace lab1\classes;
+
 class Comment
 {
     public function __construct(protected User $user, protected string $message)
@@ -15,9 +17,10 @@ class Comment
     {
         return $this->message;
     }
-    
+
     function info(): void
     {
         echo "id: {$this->user->getId()} name: {$this->user->getName()} email: {$this->user->getEmail()} password: {$this->user->getPassword()}\n" . "Message: $this->message;\n";
     }
 } 
+
